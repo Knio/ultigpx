04f9c33b60730c9008fde4f7ff7e0b9d9447e9d4
theknio <theknio@91a46f87-b747-0410-a1e8-578e7efe125a>
2008-03-08 03:20:19 +0000

    more

--- build.bat
+++ build.bat
@@ -1,1 +1,5 @@
-javac ultigpx\UGPXFile.java 
+rm -rf bin/*
+rm UltiGPX.jar
+
+javac ultigpx\UltiGPX.java -d bin
+jar cvmf Manifest.txt UltiGPX.jar -C bin .
---
+++ ultigpx/UltiGPX.java
@@ -0,0 +1,19 @@
+
+package ultigpx;
+
+import java.util.*;
+
+public class UltiGPX
+{
+    UGPXFile file;
+    public static void main(String args[])
+    {
+        new UltiGPX();
+    }
+    
+    public UltiGPX()
+    {
+        System.out.println("Hello, World!");
+    }
+    
+}
