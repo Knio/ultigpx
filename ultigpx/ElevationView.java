d835c6d4fef190b2f3bbda1187233cefab450ed5
ndsorens@ucalgary.ca <ndsorens@ucalgary.ca@91a46f87-b747-0410-a1e8-578e7efe125a>
2008-03-17 21:37:40 +0000

    Fleshed out a bit. Still not tested at all.

--- ultigpx/ElevationView.java
+++ ultigpx/ElevationView.java
@@ -16,10 +16,14 @@ public class ElevationView extends JPanel
 {
     UltiGPX         main;
     UGPXFile        file;
-    Waypoint        selected;
+    Waypoint        selected;   
     
     java.util.List<Waypoint> entities;
     
+    private double elev_low;
+    private double elev_high;
+    private double lon_low;
+    private double lon_high;
     
     public ElevationView(UltiGPX main)
     {
@@ -27,26 +31,98 @@ public class ElevationView extends JPanel
         this.main = main;
     }
     
-    protected void select(Waypoint wp)
+    /**
+     * identifies a point to be displayed as the "Selected" point
+     * @param wp the waypoint that is selected
+     */
+    public void select(Waypoint wp)
     {
         selected = wp;
         repaint();
     }
     
-    
+    private void updateBounds(Waypoint wpt) {
+    	if(wpt.getEle() > elev_high)
+			elev_high = wpt.getEle();
+		if(wpt.getEle() < elev_low)
+			elev_low = wpt.getEle();
+		if(wpt.getLon() > lon_high)
+			lon_high = wpt.getLon();
+		if(wpt.getLon() < lon_low)
+			lon_low = wpt.getLon();
+    }
+    /**
+     * Refreshes the elevation view
+     */
     public void paintComponent(Graphics g)
     {
         super.paintComponent(g);
         
         Graphics2D g2d = (Graphics2D)g;
         
-        g2d.setPaint(Color.RED);
+
+        //leave this window empty if there is no file loaded yet.
+        if (main.file == null)
+            return;
+        
+        // We'll iterate over the waypoints looking for the highest and lowest
+        // to properly scale the graph.        
+        elev_low = 999999999;
+        lon_low = 999999999;
+        elev_high = -999999999;
+        lon_high = -999999999;
+        for(Route rt : main.file.routes()) {       
+        	for(Waypoint wpt : rt) {
+        		updateBounds(wpt);
+        	}
+        }
+        for(Track trk : main.file.tracks()) {       
+        	for(TrackSegment ts : trk) {
+        		for(Waypoint wpt : ts ) {
+        			updateBounds(wpt);
+        		}
+        	}
+        }
+        for(Waypoint wpt : main.file.waypoints() ) {
+        	updateBounds(wpt);
+        }
+        
+        //The scaling factors for longitude and elevation
+        Double le = getWidth()/(lon_high-lon_low);
+        Double ls = getHeight() / (elev_high - elev_low);
+        Waypoint last = null;
+        
+        //Now we can draw the waypoints
+        for(Route rt : main.file.routes()) {       
+        	for(Waypoint wpt : rt) {
+        		if(last != null) {
+        			g2d.drawLine((int)(last.getLon()*ls), (int)(getHeight() - (last.getEle()*le)), 
+        					(int)(wpt.getLon()*ls), (int)(getHeight() - (wpt.getEle()*le)));
+        		}
+        		last = wpt;
+        	}
+        }
+        last = null;
+        for(Track trk : main.file.tracks()) {       
+        	for(TrackSegment ts : trk) {
+        		for(Waypoint wpt : ts ) {
+        			if(last != null) {
+            			g2d.drawLine((int)(last.getLon()*ls), (int)(getHeight() - (last.getEle()*le)), 
+            					(int)(wpt.getLon()*ls), (int)(getHeight() - (wpt.getEle()*le)));
+            		}
+            		last = wpt;
+        		}
+        	}
+        }
+        
+        for(Waypoint wpt : main.file.waypoints() ) {
+    			g2d.drawOval((int)(wpt.getLon()*ls), (int)(getHeight() - (wpt.getEle()*le)), 
+    					2,2);
+        }
+        
+        g2d.setPaint(Color.GREEN);
         
         g2d.draw(new Rectangle2D.Double(10, 10, getWidth()-20, getHeight()-20));
-        
-        Rectangle r = getBounds();
-        r.grow(-20,-20);
-        g2d.draw(r);
     }
 }
 
