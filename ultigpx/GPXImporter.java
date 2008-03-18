ea330648bcde38efa7ef95f10575f27768f03cf7
greenpanda <greenpanda@91a46f87-b747-0410-a1e8-578e7efe125a>
2008-03-18 18:28:52 +0000

    GPXImporter will now import routes

--- ultigpx/GPXImporter.java
+++ ultigpx/GPXImporter.java
@@ -27,15 +27,13 @@ public class GPXImporter {
             //For every child, read it in
             Iterator childListIterator = childList.iterator();
             
-            while (childListIterator.hasNext())
-            {
+            while (childListIterator.hasNext()) {
                 Element currentElement = (Element) childListIterator.next();
                 
                 //Determine the type of this element
                 
                 //If it is a waypoint
-                if (currentElement.getName().equals("wpt"))
-                {
+                if (currentElement.getName().equals("wpt")) {
                     List waypointChildList = currentElement.getChildren();
                     Iterator waypointChildIterator = waypointChildList.iterator();
                     String name = "";
@@ -43,8 +41,7 @@ public class GPXImporter {
                     double ele = 0;
                     double time = 0;
                     
-                    while(waypointChildIterator.hasNext())
-                    {
+                    while(waypointChildIterator.hasNext()) {
                         Element currentWaypointChild = (Element) waypointChildIterator.next();
                         if (currentWaypointChild.getName().equals("name"))
                             name = currentWaypointChild.getText();
@@ -54,30 +51,49 @@ public class GPXImporter {
                             ele = Double.parseDouble(currentWaypointChild.getText());
                         else if (currentWaypointChild.getName().equals("time"))
                             time = Double.parseDouble(currentWaypointChild.getText());
-                    } //end while   
+                    } //end while
                     returnValue.addWaypoint( new Waypoint(name, desc, currentElement.getAttribute("lat").getDoubleValue(), currentElement.getAttribute("lon").getDoubleValue(), ele, time));
                 } //end if waypoint
-                    
+                
                 //If it is a route
-                else if (currentElement.getName().equals("rte"))
-                {
-                    ;
-                    //Get <name> xsd:string </name> [0..1] ?
-                    //Get <cmt> xsd:string </cmt> [0..1] ?
-                    //Get <desc> xsd:string </desc> [0..1] ?
-                    //Get <src> xsd:string </src> [0..1] ?
-                    //Get <link> linkType </link> [0..*] ?
-                    //Get <number> xsd:nonNegativeInteger </number> [0..1] ?
-                    //Get <type> xsd:string </type> [0..1] ?
-                    //Get <extensions> extensionsType </extensions> [0..1] ?
-                    //Get <rtept> wptType </rtept> [0..*] ?
-                        //Copy this from above
-                    //returnValue.addRoute();
+                else if (currentElement.getName().equals("rte")) {
+                    List routeChildList = currentElement.getChildren();
+                    Iterator routeChildIterator = routeChildList.iterator();
+                    Route newRoute = new Route();
+                    
+                    while(routeChildIterator.hasNext()) {
+                        Element currentRouteChild = (Element) routeChildIterator.next();
+                        if (currentElement.getName().equals("rtept")) {
+                            List waypointChildList = currentElement.getChildren();
+                            Iterator waypointChildIterator = waypointChildList.iterator();
+                            String name = "";
+                            String desc = "";
+                            double ele = 0;
+                            double time = 0;
+                            
+                            while(waypointChildIterator.hasNext()) {
+                                Element currentWaypointChild = (Element) waypointChildIterator.next();
+                                if (currentWaypointChild.getName().equals("name"))
+                                    name = currentWaypointChild.getText();
+                                else if (currentWaypointChild.getName().equals("desc"))
+                                    desc = currentWaypointChild.getText();
+                                else if (currentWaypointChild.getName().equals("ele"))
+                                    ele = Double.parseDouble(currentWaypointChild.getText());
+                                else if (currentWaypointChild.getName().equals("time"))
+                                    time = Double.parseDouble(currentWaypointChild.getText());
+                            } //end while
+                            
+                            Waypoint newWaypoint = new Waypoint(name, desc, currentElement.getAttribute("lat").getDoubleValue(), currentElement.getAttribute("lon").getDoubleValue(), ele, time);
+                            newRoute.add(newWaypoint);
+                            
+                        } //end if waypoint
+                    } //end while
+                    
+                    returnValue.addRoute(newRoute);
                 } //end if route
                 
                 //If it is a track
-                else if (currentElement.getName().equals("trk"))
-                {
+                else if (currentElement.getName().equals("trk")) {
                     ;
                     //Get <name> xsd:string </name> [0..1] ?
                     //Get <cmt> xsd:string </cmt> [0..1] ?
@@ -88,11 +104,11 @@ public class GPXImporter {
                     //Get <type> xsd:string </type> [0..1] ?
                     //Get <extensions> extensionsType </extensions> [0..1] ?
                     //Get <trkseg> trksegType </trkseg> [0..*] ?
-    
+                    
                     //Track segment stuff
-                        //Get <trkpt> wptType </trkpt> [0..*] ?
-                        //Get <extensions> extensionsType </extensions> [0..1] ?
-    
+                    //Get <trkpt> wptType </trkpt> [0..*] ?
+                    //Get <extensions> extensionsType </extensions> [0..1] ?
+                    
                     //returnValue.addTrack(name);
                 } //end if track
                 
