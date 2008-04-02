8bb6e40530526c8c18a550927fe4df5e3df283d2
greenpanda <greenpanda@91a46f87-b747-0410-a1e8-578e7efe125a>
2008-04-02 04:59:04 +0000

    This new GPX Exporter exports to KML or GPX.  Note that the method signatures have changed.

--- ultigpx/GPXExporter.java
+++ ultigpx/GPXExporter.java
@@ -1,6 +1,6 @@
 package ultigpx;
 /**
- * Class GPX Exporter is used for Exporting the UltiGPXFile to a KML file
+ * Class GPX Exporter is used for Exporting the UltiGPXFile to a GPX file or a KML file
  * The KML file can then be view using Google Earth
  * For more information about Google Earth, see http://earth.google.com/
  * This class was written for Part 1 of the project for CPSC 301, Winter 2008
@@ -8,6 +8,7 @@ package ultigpx;
  */
 
 import java.util.*;
+import java.util.Date.*;
 import java.io.*;
 import org.jdom.*;
 import org.jdom.input.*;
@@ -15,15 +16,15 @@ import org.jdom.input.*;
 
 public class GPXExporter {
     
-        /**
-     * exportGPX exports a UGPXFile.  The filename is given as a string as well as the UGPXFile.
+    /**
+     * exportToKML exports a UGPXFile to KML.  The filename is given as a string as well as the UGPXFile.
      * If there are any problems with the file, either a JDOMException or an IO Exception is thrown
      * @param filename, a String, the name of the file you want to export
      * @param UGPXFile, containing the tracks, routes, and waypoints
-     * @throws JDOMException if a problem occurs when importing the file
-     * @throws IOException if a problem occurs when importing the file
+     * @throws JDOMException if a problem occurs when exporting the file
+     * @throws IOException if a problem occurs when exporting the file
      */
-    public static void exportGPX(UGPXFile outputData, String filename) throws JDOMException, IOException {
+    public static void exportToKML(UGPXFile outputData, String filename) throws JDOMException, IOException {
         
         //Attributes
         BufferedWriter outputFile = new BufferedWriter(new FileWriter(filename));
@@ -31,7 +32,6 @@ public class GPXExporter {
         //Output KML header
         outputFile.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
         outputFile.write("<kml xmlns=\"http://earth.google.com/kml/2.2\">\n");
-        outputFile.write("\t<Document>\n");
         
         //Put waypoints in the file
         List<Waypoint> waypointsToExport = outputData.waypoints();
@@ -111,8 +111,8 @@ public class GPXExporter {
                     outputFile.write("\t\t</Placemark>\n");
                     outputFile.write("\t</Document>\n");
                     
-                } //end if getEnabled  
-            } //end while 
+                } //end if getEnabled
+            } //end while
         } //end if
         
         
@@ -167,11 +167,167 @@ public class GPXExporter {
         } //end if
         
         //Write footer
-        outputFile.write("\t</Document>\n");
         outputFile.write("</kml>");
         
         //Close input file
         outputFile.close();
         
     } //end exportGPX
+    
+    /**
+     * exportToGPX exports a UGPXFile to GPX.  The filename is given as a string as well as the UGPXFile.
+     * If there are any problems with the file, either a JDOMException or an IO Exception is thrown
+     * @param filename, a String, the name of the file you want to export
+     * @param UGPXFile, containing the tracks, routes, and waypoints
+     * @throws JDOMException if a problem occurs when exporting the file
+     * @throws IOException if a problem occurs when exporting the file
+     */
+    public static void exportToGPX(UGPXFile outputData, String filename) throws JDOMException, IOException {
+        
+        //Attributes
+        BufferedWriter outputFile = new BufferedWriter(new FileWriter(filename));
+        
+        //Output KML header
+        
+        
+        outputFile.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\" ?>\n");
+        outputFile.write("<gpx creator=\"UltiGPX\" version=\"1.1\">\n");
+        outputFile.write("<metadata> metadataType </metadata>\n");
+        
+        //Put waypoints in the file
+        List<Waypoint> waypointsToExport = outputData.waypoints();
+        if (waypointsToExport != null) {
+            //For each waypoint
+            Iterator<Waypoint> waypointIterator = waypointsToExport.iterator();
+            while (waypointIterator.hasNext()) {
+                //Export each enabled waypoint
+                Waypoint currentWaypoint = waypointIterator.next();
+                if (currentWaypoint.getEnabled()) {
+                    
+                    //Print out waypoint
+                    outputFile.write("<wpt");
+                    outputFile.write("lat=\"" + currentWaypoint.getLat() + "\" ");
+                    outputFile.write("lon=\"" + currentWaypoint.getLon() + "\" >\n");
+                    outputFile.write("<ele>" + currentWaypoint.getEle() + "</ele>\n");
+                    outputFile.write("<time>" + convertTime(currentWaypoint.getTime()) + "</time>\n");
+                    outputFile.write("<name>" + currentWaypoint.getName() + "</name>\n");
+                    outputFile.write("<desc>" + currentWaypoint.getDesc() + "</desc>\n");
+                    outputFile.write("</wpt>\n");
+                } //end if getEnabled
+            } //end while
+        } //end if
+        
+        //Put tracks in the file
+        List<Track> tracksToExport = outputData.tracks();
+        if (tracksToExport != null) {
+            //For each track
+            Iterator<Track> tracksIterator = tracksToExport.iterator();
+            while (tracksIterator.hasNext()) {
+                //Export each enabled track
+                Track currentTrack = tracksIterator.next();
+                if (currentTrack.getEnabled()){
+                    
+                    //Print out header stuff
+                    
+                    outputFile.write("<trk>\n");
+                    outputFile.write("<name>" + currentTrack.getName() + "</name>\n");
+                    
+                    //Print out all coordinates
+                    for (int counter = 0; counter < currentTrack.size(); counter++) {
+                        TrackSegment currentTrackSegment = currentTrack.get(counter);
+                        outputFile.write("<trkseg>\n");
+                        for(int counter2 = 0; counter2 < currentTrackSegment.size(); counter2++) {
+                            Waypoint currentWaypoint = currentTrackSegment.get(counter2);
+                            outputFile.write("<trkpt");
+                            outputFile.write("lat=\"" + currentWaypoint.getLat() + "\" ");
+                            outputFile.write("lon=\"" + currentWaypoint.getLon() + "\" >\n");
+                            outputFile.write("<ele>" + currentWaypoint.getEle() + "</ele>\n");
+                            outputFile.write("<time>" + convertTime(currentWaypoint.getTime()) + "</time>\n");
+                            outputFile.write("<name>" + currentWaypoint.getName() + "</name>\n");
+                            outputFile.write("<desc>" + currentWaypoint.getDesc() + "</desc>\n");
+                            outputFile.write("</trkpt>\n");
+                        } //end for
+                        outputFile.write("</trkseg>");
+                    } //end for
+                    
+                    //Print out footer stuff
+                    outputFile.write("</trk>");
+                    
+                } //end if getEnabled
+            } //end while
+        } //end if
+        
+        
+        //Put routes in file
+        List<Route> routesToExport = outputData.routes();
+        if (routesToExport != null) {
+            //For each route
+            Iterator<Route> routesIterator = routesToExport.iterator();
+            while (routesIterator.hasNext()) {
+                //Export each enabled route
+                Route currentRoute = routesIterator.next();
+                if (currentRoute.getEnabled()) {
+                    
+                    //Print out header
+                    
+                    //Print out header stuff
+                    
+                    outputFile.write("<rte>\n");
+                    outputFile.write("<name>" + currentRoute.getName() + "</name>\n");
+                    
+                    //Print out all coordinates
+                    for (int counter = 0; counter < currentRoute.size(); counter++) {
+                            Waypoint currentWaypoint = currentRoute.get(counter);
+                            outputFile.write("<rtept");
+                            outputFile.write("lat=\"" + currentWaypoint.getLat() + "\" ");
+                            outputFile.write("lon=\"" + currentWaypoint.getLon() + "\" >\n");
+                            outputFile.write("<ele>" + currentWaypoint.getEle() + "</ele>\n");
+                            outputFile.write("<time>" + convertTime(currentWaypoint.getTime()) + "</time>\n");
+                            outputFile.write("<name>" + currentWaypoint.getName() + "</name>\n");
+                            outputFile.write("<desc>" + currentWaypoint.getDesc() + "</desc>\n");
+                            outputFile.write("</rtept>\n");
+                    } //end for
+                    
+                    //Print out footer stuff
+                    outputFile.write("</rte>");
+                    
+                } //end if getEnabled
+            } //end while
+        } //end if
+        
+        //Write footer
+        outputFile.write("</kml>");
+        
+        //Close input file
+        outputFile.close();
+        
+    } //end exportGPXToGPX
+    
+    /**
+     * Convert Time extracks the time and converts it to a string to be exported in the GPXFile.
+     * Time in a GPX file is stored as YYYY_MM_DDTHH:MM:SSZ
+     * This method uses a Gregorian calendar to convert time in milis into the string.
+     * This method is private because it is only used when exporting GPX files
+     * @param toConvert, a double, the time in milis since 1970
+     * @returns String, a string representing time in the GPX file
+     */
+    private static String convertTime(double toConvert) {
+        
+        //Create a calender for converting times
+        GregorianCalendar timeConverter = new GregorianCalendar();
+        timeConverter.setGregorianChange(new Date((long) toConvert));
+        
+        //Other attributes
+        Integer year = timeConverter.get(Calendar.YEAR);
+        Integer month = timeConverter.get(Calendar.MONTH);
+        Integer day = timeConverter.get(Calendar.DATE);
+        Integer hour = timeConverter.get(Calendar.HOUR);
+        Integer minute = timeConverter.get(Calendar.MINUTE);
+        Integer second = timeConverter.get(Calendar.SECOND);
+        
+        //Return the date string
+        return year + "-" + month + "-" + day + "T" + hour + ":" + minute + ":" + second + "Z";
+    } //end getTime
+    
+    
 } //end GPXExporter
